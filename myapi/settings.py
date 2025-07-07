a9e9985a42e05083f88e09ac0e82acc15c68db9e
mayur261202 <mayur.tis400@gmail.com>
2025-07-07 14:11:04 +0530

    updated allowed host setting

--- myapi/settings.py
+++ myapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--k$=l$48owt8&lu#&+*!a(=c-qr)g@bu2w@=42&_(0#-dy=*56
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
